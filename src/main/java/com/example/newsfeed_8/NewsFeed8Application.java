90054e92d6c14cb90e9039425a02712c5498428f
k-jaehyun <wogus146@gmail.com>
2023-11-22 10:30:09 +0900

    init commit

---
+++ src/main/java/com/example/newsfeed_8/NewsFeed8Application.java
@@ -0,0 +1,13 @@
+package com.example.newsfeed_8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsFeed8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsFeed8Application.class, args);
+	}
+
+}
