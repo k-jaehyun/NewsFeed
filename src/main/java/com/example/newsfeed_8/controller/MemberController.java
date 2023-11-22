02cb65f7ffcf0300c238f91da063733cc4e31e8b
Yoon <dlstjr9390@gmail.com>
2023-11-22 15:48:42 +0900

    URL 수정

--- src/main/java/com/example/newsfeed_8/controller/MemberController.java
+++ src/main/java/com/example/newsfeed_8/controller/MemberController.java
@@ -1,7 +1,7 @@
 package com.example.newsfeed_8.controller;
 
 import com.example.newsfeed_8.dto.CommonResponseDto;
-import com.example.newsfeed_8.dto.MeberRequestDto;
+import com.example.newsfeed_8.dto.MemberRequestDto;
 import com.example.newsfeed_8.jwt.JwtUtil;
 import com.example.newsfeed_8.service.MemberService;
 import jakarta.servlet.http.HttpServletResponse;
@@ -15,15 +15,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/member")
 @RequiredArgsConstructor
 public class MemberController {
     private final MemberService memberService;
     private final JwtUtil jwtUtil;
-    @PostMapping("/no-auth/member")
-    public ResponseEntity<CommonResponseDto> signup(@Valid @RequestBody MeberRequestDto meberRequestDto){
+    @PostMapping("")
+    public ResponseEntity<CommonResponseDto> signup(@Valid @RequestBody MemberRequestDto memberRequestDto){
         try {
-            memberService.signup(meberRequestDto);
+            memberService.signup(memberRequestDto);
         } catch (IllegalArgumentException e) {
             return ResponseEntity.badRequest()
                     .body(new CommonResponseDto("중복된 아이디 입니다.", HttpStatus.BAD_REQUEST.value()));
@@ -32,17 +32,19 @@ public class MemberController {
                 .body(new CommonResponseDto("회원가입 성공", HttpStatus.CREATED.value()));
     }
 
-    @PostMapping("/no-auth/member/login")
-    public ResponseEntity<CommonResponseDto> login(@RequestBody MeberRequestDto meberRequestDto, HttpServletResponse res){
+    @PostMapping("/login")
+    public ResponseEntity<CommonResponseDto> login(@RequestBody MemberRequestDto memberRequestDto, HttpServletResponse res){
         try {
-            memberService.login(meberRequestDto);
+            memberService.login(memberRequestDto);
         } catch (IllegalArgumentException e) {
             return ResponseEntity.badRequest()
                     .body(new CommonResponseDto(e.getMessage(), HttpStatus.BAD_REQUEST.value()));
         }
 
-        res.setHeader(JwtUtil.AUTHORIZATION_HEADER, jwtUtil.createToken(meberRequestDto.getUserId()));
+        res.setHeader(JwtUtil.AUTHORIZATION_HEADER, jwtUtil.createToken(memberRequestDto.getUserId()));
 
         return ResponseEntity.ok().body(new CommonResponseDto("로그인 성공", HttpStatus.OK.value()));
     }
+
+
 }
